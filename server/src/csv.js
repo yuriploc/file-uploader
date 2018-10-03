3918b7af95981955a87fcf83745b658ba3c9558d
Yuri Oliveira <soaresyuri@gmail.com>
2018-10-03 19:35:25 -0300

    csv module

---
+++ server/src/csv.js
@@ -0,0 +1,23 @@
+const parse = require('csv-parse');
+
+const csv = parse({
+  skip_empty_lines: true,
+  trim: true,
+  columns: true
+});
+
+let out = [];
+
+csv.on('readable', () => {
+  let record = csv.read();
+  while (record) {
+    out.push(record);
+    record = csv.read();
+  }
+});
+
+csv.on('end', _ => {
+  console.log(out);
+});
+
+module.exports = csv;
