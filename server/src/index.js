b9af3c3bfbe1443189e4d38771085f41edb6f215
Yuri Oliveira <soaresyuri@gmail.com>
2018-10-03 18:55:58 -0300

    feat: handle file in memory and parse it with csv-parse
    * add csv-parse
    * add knex for migrations

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const parse = require('csv-parse');
 
 const app = express();
 
 const PORT = 3001;
-
-const storage = multer.diskStorage({
-  dest: './temp',
-  filename(req, file, cb) {
-    cb(null, `${file.originalname}-${Date.now()}`);
-  }
-});
-
-const upload = multer({ storage });
+const upload = multer();
 
 app.post('/uploads', cors(), upload.single('file'), (req, res) => {
   const file = req.file;
   const meta = req.body;
+  let saida = [];
+  let count = 0;
 
-  console.log(file);
+  // console.log(file);
+  // console.log(meta);
+  const csv = parse({
+    skip_empty_lines: true,
+    trim: true,
+    columns: true
+  });
+
+  csv.write(file.buffer);
+  csv.end();
+
+  csv.on('readable', () => {
+    let record = csv.read();
+    while (record) {
+      saida.push(record);
+      record = csv.read();
+    }
+  });
+
+  csv.on('end', something => {
+    console.log(saida);
+  });
 
   // TODO: persist on pg
   res.status(200).send({ uploadId: 8298 });
