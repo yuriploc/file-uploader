221c393bb74fcda4a5da9ff7f739fec4a97385b8
Yuri Oliveira <soaresyuri@gmail.com>
2018-10-04 02:30:26 -0300

    chore: back to re-bulma for list page

--- client/src/components/UploadFile.js
+++ client/src/components/UploadFile.js
@@ -1,37 +1,18 @@
 import React from 'react';
-import { Button, Form, Message } from 'semantic-ui-react';
 import { BASE_URL } from '../constants/api';
 import AlertBox from './AlertBox';
 
 class UploadFile extends React.Component {
-  // fileName = React.createRef();
-  // file = React.createRef();
+  fileName = React.createRef();
+  file = React.createRef();
 
-  state = {
-    isSent: false,
-    boxStyle: '',
-    boxMsg: '',
-    fileNameChange: '',
-    fileChange: '',
-    fileName: '',
-    file: ''
-  };
-
-  handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value });
-  };
+  state = { isSent: false, boxStyle: '', boxMsg: '' };
 
   handleUpload = event => {
     event.preventDefault();
-    const { fileNameChange, fileChange } = this.state;
-    this.setState({ file: fileChange, fileName: fileNameChange });
-    // console.log(fileName);
-    // // console.log(file);
-    console.log(fileNameChange);
-    console.log(fileChange);
     const formData = new FormData();
-    formData.append('filename', this.state.fileName.current.value);
-    formData.append('file', this.state.file.current.files[0]);
+    formData.append('filename', this.fileName.current.value);
+    formData.append('file', this.file.current.files[0]);
     fetch(`${BASE_URL}/uploads`, {
       method: 'POST',
       body: formData
@@ -61,44 +42,19 @@ class UploadFile extends React.Component {
 
   // TODO: CSS!
   render() {
-    const { fileNameChange, fileChange } = this.state;
     return (
       <React.Fragment>
-        <Form encType="multipart/form-data" onSubmit={this.handleUpload}>
-          <Form.Field>
-            <input
-              type="text"
-              defaultValue={fileNameChange}
-              name="filename"
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Form.Field>
-            <input
-              type="file"
-              defaultValue={fileChange}
-              name="file"
-              accept="text"
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Button type="submit" onClick={this.handleUpload}>
-            Submit
-          </Button>
-        </Form>
+        <form encType="multipart/form-data" onSubmit={this.handleUpload}>
+          <input type="text" ref={this.fileName} name="filename" />
+          <br />
+          <input type="file" ref={this.file} name="file" accept="text" />
+          <br />
+          <button type="submit">Upload File</button>
+        </form>
+        {this.state.isSent && (
+          <AlertBox boxStyle={this.state.boxStyle} boxMsg={this.state.boxMsg} />
+        )}
       </React.Fragment>
-      // <React.Fragment>
-      //   <form encType="multipart/form-data" onSubmit={this.handleUpload}>
-      //     <input type="text" ref={this.fileName} name="filename" />
-      //     <br />
-      //     <input type="file" ref={this.file} name="file" accept="text" />
-      //     <br />
-      //     <button type="submit">Upload File</button>
-      //   </form>
-      //   {this.state.isSent && (
-      //     <AlertBox boxStyle={this.state.boxStyle} boxMsg={this.state.boxMsg} />
-      //   )}
-      // </React.Fragment>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Router from './components/Router';
-import 'semantic-ui-css/semantic.min.css';
 
 render(<Router />, document.getElementById('main'));
